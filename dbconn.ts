4fc015d5978ecdb1d1c16106738d4a4bb39570b6
aumZax <oodgame.1h@gmail.com>
2024-03-09 14:35:29 +0700

    db update online

--- dbconn.ts
+++ dbconn.ts
@@ -2,8 +2,13 @@ import mysql from "mysql";
 
 export const conn = mysql.createPool({
   connectionLimit: 10,
-  host: "localhost",
-  user: "demo",
-  password: "abc123",
-  database: "work5pro",
+  // host: "localhost",
+  // user: "demo",
+  // password: "abc123",
+  // database: "work5pro",
+
+   host: "sql6.freemysqlhosting.net",
+  user: "sql6689818",
+  password: "LqPfV95nnJ",
+  database: "sql6689818",
 });
